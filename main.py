eadbc01cf9ad6fdb545274b256e3c27bce37ef2b
jonantanmmartin2 <aimarttin@gmail.com>
2023-09-29 14:43:12 -0500

    creacion de la base de datos de la biblioteca de autores con sus libros y fecha de publicacion

---
+++ main.py
@@ -0,0 +1,24 @@
+biblioteca = [
+    {
+        'autor': 'gabriel garcia marquez',
+        'libros': {
+            'título': ['cien años de soledad', 'el amor en los tiempos de cólera', 'memorias de mis putas trites' ],
+            'fecha': ['1967', '1967', '2004']
+        }
+    },
+    {
+        'autor': 'oscar wilde',
+        'libros': {
+            'título': ['el retrato de dorian gray', 'el fantasma de canterville', 'el principe feliz'],
+            'fecha' : ['1890', '1887', '1888']
+        }
+    },
+    {
+        'autor': 'marguerite duras',
+        'libros': {
+            'título': ['el amante', 'hiroshima mon amour', 'el dolor'],
+            'fecha': ['1984', '1959', '1985']
+        }
+    }
+]
+
