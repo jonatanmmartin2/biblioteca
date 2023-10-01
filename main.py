124d7b99cd57e3e8d31725c41948c347858f9fce
jonantanmmartin2 <aimarttin@gmail.com>
2023-09-30 19:22:08 -0500

    creacion de funcion para ver los autores, sus libros y fecha de publicacion. Por ahora esta en desarrollo la funcion de ver individualmente a un autor y sus libros

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ biblioteca = [
     {
         'autor': 'gabriel garcia marquez',
         'libros': {
-            'título': ['cien años de soledad', 'el amor en los tiempos de cólera', 'memorias de mis putas trites' ],
+            'título': ['cien años de soledad', 'el amor en los tiempos de cólera', 'memorias de mis putas tristes' ],
             'fecha': ['1967', '1967', '2004']
         }
     },
@@ -22,3 +22,46 @@ biblioteca = [
     }
 ]
 
+def mostrar_libros():
+    for info_autor in biblioteca:
+        autor = info_autor['autor'].upper() 
+        
+        libros = info_autor['libros']['título']
+        fechas = info_autor['libros']['fecha']
+
+        interfaz = '█' * 70
+        divisor = '-' * 70
+        
+        print(f'{interfaz}\n{divisor}\n{autor}\n{divisor}')
+
+        for i in range(len(libros)):
+            libro = libros[i]
+            fecha = fechas[i]
+            print(f'Libro: {libro.title()}, Publicado en : {fecha}')
+
+    print('█' * 70)
+
+def mostrar_libros_por_autor():
+    nombre_autor = input('ingrese el nombre del autor: ').lower()
+
+    for libros_autor in biblioteca:
+        autor = libros_autor['autor']
+        libros = libros_autor['libros']['título']
+        fechas = libros_autor['libros']['fecha']
+
+        interfaz = '█' * 70
+        divisor = '-' * 70
+        
+        if (nombre_autor == autor) == True:
+            for i in range(len(libros)):
+                libro = libros[i]
+                fecha = fechas[i]
+                print(f'{interfaz}\n{divisor}\n{autor}\n{divisor}\nLibro: {libro}, Publicado en: {fecha}')
+            break
+        elif (nombre_autor == autor) == False:
+            continue
+        else:
+            print('autor no encontrado en la biblioteca')
+            break
+
+mostrar_libros_por_autor()
